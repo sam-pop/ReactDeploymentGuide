d7ae35c4563e15e773d7c253a846abc7fe1c864a
Samuel Poplovitch <poplovitch.samuel@gmail.com>
2018-10-26 13:06:14 -0400

    test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            DEPLOY REACT
+            DEPLOY REACT!
           </a>
         </header>
       </div>
