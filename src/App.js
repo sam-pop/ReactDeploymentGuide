64559e6d18c8b2a4de1a64294171a6723fd3a785
Sam Pop <poplovitch.samuel@gmail.com>
2018-10-31 11:38:39 -0400

    updated deployed app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./anim.gif";
 import "./App.css";
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>This is how you</p>
+          <p>This is how you successfully </p>
           DEPLOY REACT APP TO GITHUB-PAGES!
         </header>
       </div>
